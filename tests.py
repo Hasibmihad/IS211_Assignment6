68ed7b154fed741c0804e001f241f9066be2b45f
Hasibmihad <mdhasibuddin12@gmail.com>
2023-09-06 15:33:57 +0600

    ITEM  1 test

---
+++ tests.py
@@ -0,0 +1,11 @@
+import conversions
+
+"""
+result = conversions.convertCelsiusToFahrenheit(300.00)
+assert result == 572.00, f"Expected 572.00°F but got {result}°F"
+
+
+result = conversions.convertCelsiusToKelvin(300.00)
+assert result == 573.15, f"Expected -40.00°F but got {result}°F" """
+
+

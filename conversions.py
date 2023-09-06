4dbe356b2ab9af3ad733fad8306705b4a048cba3
Hasibmihad <mdhasibuddin12@gmail.com>
2023-09-06 15:37:11 +0600

    6 functions done

--- conversions.py
+++ conversions.py
@@ -1,9 +1,28 @@
 
 def convertCelsiusToKelvin(celsius):
-     kelvin = celsius + 273.15
-     return 0.0
+     kelvin = float(celsius) + 273.15
+     return float(kelvin)
 
-"Function converts C to F"
+
 def convertCelsiusToFahrenheit(celsius):
-     fahrenheit = (celsius * 9) / 5 + 32
-     return 0.0
+     fahrenheit = (float(celsius) * 9) / 5 + 32
+     return float(fahrenheit)
+
+
+def convertFahrenheitToCelsius(fahrenheit):
+     celsius = ((float(fahrenheit) - 32) * 5) / 9
+     return float(celsius)
+
+def convertFahrenheitToKelvin(fahrenheit):
+     kelvin = (float(fahrenheit) + 459.67) * 5/9
+     return float(kelvin)
+
+
+def convertKelvinToCelsius(kelvin):
+     celsius = float(kelvin) - 273.15
+     return float(celsius)
+
+
+def convertKelvintoFahrenheit(kelvin):
+     fahrenheit = (float(kelvin) * 9/5) - 459.67
+     return float(fahrenheit)
